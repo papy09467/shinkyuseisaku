267ce192550a779a33e154d6da48e204937f523d
papy09467 <papy09467@gmail.com>
2016-09-23 14:41:28 +0900

    スタミナゲージ途中

--- aquatimeescape/Assets/Scripts/Stamina.cs
+++ aquatimeescape/Assets/Scripts/Stamina.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Stamina : MonoBehaviour {
+	public  float maxStaminaNum;               // スタミナの上限数
+	float nowStaminaNum;               // 現在のスタミナ数
+	float StaminaDown = 1;
 
-	// Use this for initialization
+	Slider slider1;
+
 	void Start () {
-	
+		slider1 = GameObject.Find ("Slider").GetComponent<Slider>();
+		nowStaminaNum = maxStaminaNum;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.A)) {
+			nowStaminaNum -= StaminaDown;
+		} else if(nowStaminaNum < maxStaminaNum){
+			nowStaminaNum += StaminaDown;
+		}
+
+		slider1.value = nowStaminaNum;
 	}
+
+		
 }
