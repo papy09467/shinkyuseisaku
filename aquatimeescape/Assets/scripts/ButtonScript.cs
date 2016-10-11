5376bc5864fa999b1d1e46f9b10e4ca7282e66ed
ikumiokajima <ikumi.okajima@gmail.com>
2016-10-11 09:49:14 +0900

    タイトルのUI配置とタイトルからトレーニング画面への遷移

---
+++ aquatimeescape/Assets/scripts/ButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void MultiPlayerBtn(){
+		//CallScript.Scene("");
+	}
+	public void TrainingBtn(){
+		CallScript.Scene("training_wait");
+	}
+	public void OptionBtn(){
+		//CallScript.Scene ("");
+	}
+}
---
+++ aquatimeescape/Assets/scripts/CallScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+//シーン移動やオーディオの再生を行うスクリプト
+public class CallScript : MonoBehaviour {
+
+	public static void Scene(string sceneName){
+		SceneManager.LoadScene (sceneName);
+	}
+}
