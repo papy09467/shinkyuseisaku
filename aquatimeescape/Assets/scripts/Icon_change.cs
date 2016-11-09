8eeabd5b5b84c43e50c627cbd5ede3844adc1bc8
saaaaaaagi <sagiharatoshiki0809@gmail.com>
2016-11-09 11:06:30 +0900

    icon change

---
+++ aquatimeescape/Assets/scripts/Icon_change.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Icon_change : MonoBehaviour {
+	InputManager inputManager;
+	Image MainImage;
+
+	public Sprite DefaultSprite;
+	public Sprite PoisonSprite;
+	public Sprite DeathSprite;
+
+
+	void Start () {
+		inputManager = FindObjectOfType<InputManager> ();
+		//このobjectのImageを取得
+		MainImage = gameObject.GetComponent<Image> ();
+	}
+
+	//何かしらのタイミングで呼ばれる
+	//通常の状態
+	void ChangeStateToDefault() {
+		MainImage.sprite = DefaultSprite;
+	}
+
+	//毒状態
+	void ChangeStateToPoison() {
+		MainImage.sprite = PoisonSprite;
+	}
+
+	//死状態
+	void ChangeStateToDeath() {
+		MainImage.sprite = DeathSprite;
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Q)) {
+			ChangeStateToDefault ();
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			ChangeStateToPoison ();
+		}
+
+		if (Input.GetKey (KeyCode.E)) {
+			ChangeStateToDeath ();
+		}
+	}
+}
