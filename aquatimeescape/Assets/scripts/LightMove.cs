ffd830e1f9444e186094ce5ef0dc03b64d64d024
papy11874 <papy118748989@gmail.com>
2016-11-28 02:07:52 +0900

    波紋準完成
    あとはテクスチャを修正

---
+++ aquatimeescape/Assets/scripts/LightMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightMove : MonoBehaviour {
+
+	Transform myTransform;
+	Vector3 m_v3Position;
+
+	public Vector3 m_v3Speed;
+
+	// Use this for initialization
+	void Start () {
+
+		myTransform = transform;
+		m_v3Position = myTransform.localPosition;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//m_v3Position += m_v3Speed;
+		myTransform.localPosition += m_v3Speed;
+
+	}
+}
