01d18dec1e94a3d01b8293cee950a5218bcd2669
saaaaaaagi <sagiharatoshiki0809@gmail.com>
2016-11-29 12:01:14 +0900

    ち

--- aquatimeescape/Assets/Scripts/Kumanomi.cs
+++ aquatimeescape/Assets/Scripts/Kumanomi.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class Kumanomi : MonoBehaviour {
 
 	public GameObject deathEff; //クマノミ死エフェクト
-	private float timer;		//クマノミが消えるまでの時間を格納
-	private bool death = false; //クマノミがサメにあたったか判定
 
 	//CharacterController _controller;
 	void Start () {
@@ -14,19 +12,13 @@ public class Kumanomi : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//クマノミ削除処理
-		if (death == true && Time.time - timer > 2) {
-			Destroy (gameObject);
-			death = false;
-		}
 
 	}
 
 	void OnCollisionEnter(Collision collision){
-		if (collision.gameObject.name == "collider") {
-			timer = Time.time;
+		if (collision.gameObject.name == "shakn") {
 			Instantiate (deathEff, transform.position, Quaternion.identity);
-			death = true;
+			Destroy (gameObject);
 		}
 	}
 }
