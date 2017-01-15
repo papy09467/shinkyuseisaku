0c5c8ea1a3f629e3c3aba4ba94faf05cbe2ab0e6
papy11874 <papy118748989@gmail.com>
2017-01-16 00:04:20 +0900

    トレーニング修復

--- aquatimeescape/Assets/scripts/FollowCamera.cs
+++ aquatimeescape/Assets/scripts/FollowCamera.cs
@@ -37,8 +37,7 @@ public class FollowCamera : MonoBehaviour {
 		if (lookTarget != null && cameracheck == false) {
 			Vector3 lookPosition = lookTarget.position + offset;
 			Vector3 relativePos = Quaternion.Euler (lookTarget.eulerAngles.x, lookTarget.eulerAngles.y, 0) * new Vector3 (0, 6, -distance * 2);
-			transform.position =lookPosition + (relativePos / 4
-			) ;
+			transform.position =lookPosition + (relativePos / 2) ;
 			transform.LookAt (lookPosition);
 
 		}
